4655bed969f228f6e126df6a654ffe171550ee42
Reshaflame <reshaflame@gmail.com>
2025-06-14 17:39:27 +0300

    updated features and constants to use the enhanced preprocessed data for training

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,4 +1,4 @@
 # checked
 # src/utils/constants.py
-CHUNKS_LABELED_PATH = "data/labeled_data/chunks"
+CHUNKS_LABELED_PATH = "data/chunks_labeled_phase2"
 CHUNKS_UNLABELED_PATH = "/app/models/chunks_unlabeled"
